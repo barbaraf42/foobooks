192f278b0e76964bbfdb33390eb5528e7d1057a5
Barbara Finkemeyer <barbara.finkemeyer@gmail.com>
2017-04-12 23:53:16 -0400

    databases

--- routes/web.php
+++ routes/web.php
@@ -89,3 +89,56 @@ if (config('app.env') == 'local') {
 
 # search
 Route::get('/search', 'BookController@search');
+
+
+
+# database connection testing
+Route::get('/debug', function() {
+
+	echo '<pre>';
+
+	echo '<h1>Environment</h1>';
+	echo App::environment().'</h1>';
+
+	echo '<h1>Debugging?</h1>';
+	if(config('app.debug')) echo "Yes"; else echo "No";
+
+	echo '<h1>Database Config</h1>';
+    	echo 'DB defaultStringLength: '.Illuminate\Database\Schema\Builder::$defaultStringLength;
+    	/*
+	The following commented out line will print your MySQL credentials.
+	Uncomment this line only if you're facing difficulties connecting to the database and you
+        need to confirm your credentials.
+        When you're done debugging, comment it back out so you don't accidentally leave it
+        running on your production server, making your credentials public.
+        */
+	//print_r(config('database.connections.mysql'));
+
+	echo '<h1>Test Database Connection</h1>';
+	try {
+		$results = DB::select('SHOW DATABASES;');
+		echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+		echo "<br><br>Your Databases:<br><br>";
+		print_r($results);
+	}
+	catch (Exception $e) {
+		echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+	}
+
+	echo '</pre>';
+
+});
+
+
+# in case of database migration errors 
+if(App::environment('local')) {
+
+    Route::get('/drop', function() {
+
+        DB::statement('DROP database foobooks');
+        DB::statement('CREATE database foobooks');
+
+        return 'Dropped foobooks; created foobooks.';
+    });
+
+};
