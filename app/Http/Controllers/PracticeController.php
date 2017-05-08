2b2d245c26b7e0a95930ab83c923020ae10c5302
Barbara Finkemeyer <barbara.finkemeyer@gmail.com>
2017-05-08 10:43:42 -0400

    models and seeders

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -3,9 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
 
 class PracticeController extends Controller
 {
+
+
+    public function practice4() {
+
+        $book = new Book();
+        $books = $book->where('title', 'LIKE', '%Harry Potter%')->get();
+
+        if($books->isEmpty()) {
+            dump('No matches found');
+        }
+        else {
+            foreach($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+
+
+    public function practice3() {
+
+        # Instantiate a new Book Model object
+        $book = new Book();
+
+        # Set the parameters
+        # Note how each parameter corresponds to a field in the table
+        $book->title = "Harry Potter and the Sorcerer's Stone";
+        $book->author = 'J.K. Rowling';
+        $book->published = 1997;
+        $book->cover = 'http://prodimage.images-bn.com/pimages/9780590353427_p0_v1_s484x700.jpg';
+        $book->purchase_link = 'http://www.barnesandnoble.com/w/harry-potter-and-the-sorcerers-stone-j-k-rowling/1100036321?ean=9780590353427';
+
+        # Invoke the Eloquent `save` method to generate a new row in the
+        # `books` table, with the above data
+        $book->save();
+
+        dump('Added: '.$book->title);
+    }
+
 
 
     public function exampleX() {
@@ -51,6 +90,9 @@ class PracticeController extends Controller
         else
             dd("Practice route [{$n}] not defined");
     }
+
+
+
 
 
 
